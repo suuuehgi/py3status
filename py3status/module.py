7385d58a08218918b534894e1e0351726ee57225
lasers <lasers@users.noreply.github.com>
2018-04-29 02:01:07 -0500

    module: add background and border colors

--- py3status/module.py
+++ py3status/module.py
@@ -307,8 +307,9 @@ class Module:
         Set universal module options to be interpreted by i3bar
         https://i3wm.org/i3status/manpage.html#_universal_module_options
         """
-        self.i3s_module_options = {}
-        self.py3_module_options = {}
+        self.i3bar_module_options = {}
+        self.i3bar_gaps_module_options = {}
+        self.py3status_module_options = {}
         mod_config = self.config["py3_config"].get(module, {})
 
         if "min_width" in mod_config:
@@ -317,7 +318,7 @@ class Module:
                 err = 'invalid "min_width" attribute should be an int'
                 raise TypeError(err)
 
-            self.py3_module_options["min_width"] = min_width
+            self.py3status_module_options["min_width"] = min_width
             self.random_int = randint(0, 1)
 
             if "align" in mod_config:
@@ -330,7 +331,7 @@ class Module:
                     err += " left, center, right"
                     raise ValueError(err)
 
-                self.py3_module_options["align"] = align
+                self.py3status_module_options["align"] = align
 
         if "separator" in mod_config:
             separator = mod_config["separator"]
@@ -338,7 +339,7 @@ class Module:
                 err = 'invalid "separator" attribute, should be a bool'
                 raise TypeError(err)
 
-            self.i3s_module_options["separator"] = separator
+            self.i3bar_module_options["separator"] = separator
 
         if "separator_block_width" in mod_config:
             sep_block_width = mod_config["separator_block_width"]
@@ -347,7 +348,41 @@ class Module:
                 err += "should be an int"
                 raise TypeError(err)
 
-            self.i3s_module_options["separator_block_width"] = sep_block_width
+            self.i3bar_module_options["separator_block_width"] = sep_block_width
+
+        if "background" in mod_config:
+            background = mod_config["background"]
+            if not isinstance(background, basestring):
+                raise TypeError(
+                    "Invalid attribute `background`, "
+                    "should be a color. Got `{}`".format(background)
+                )
+
+            self.i3bar_gaps_module_options["background"] = Py3(self)._get_color(
+                background
+            )
+
+        if "border" in mod_config:
+            border = mod_config["border"]
+            if not isinstance(border, basestring):
+                raise TypeError(
+                    "Invalid attribute `border`, "
+                    "should be a color. Got `{}`".format(border)
+                )
+
+            self.i3bar_gaps_module_options["border"] = Py3(self)._get_color(border)
+
+            borders = ["top", "right", "bottom", "left"]
+            for name in ["border_" + x for x in borders]:
+                param = 1
+                if name in mod_config:
+                    param = mod_config[name]
+                    if not isinstance(param, int):
+                        raise TypeError(
+                            "Invalid attribute `{}`, "
+                            "should be an int. Got `{}`".format(name, param)
+                        )
+                self.i3bar_gaps_module_options[name] = param
 
     def process_composite(self, response):
         """
@@ -375,10 +410,11 @@ class Module:
             err = 'conflicting "full_text" and "composite" in response'
             raise Exception(err)
         # set universal options on last component
-        composite[-1].update(self.i3s_module_options)
+        composite[-1].update(self.i3bar_module_options)
         # update all components
         color = response.get("color")
         urgent = response.get("urgent")
+        composite_length = len(response["composite"]) - 1
         for index, item in enumerate(response["composite"]):
             # validate the response
             if "full_text" not in item:
@@ -389,7 +425,7 @@ class Module:
                 item["instance"] = "{} {}".format(self.module_inst, instance_index)
                 item["name"] = self.module_name
             # hide separator for all inner components unless existing
-            if index != len(response["composite"]) - 1:
+            if index != composite_length:
                 if "separator" not in item:
                     item["separator"] = False
                     item["separator_block_width"] = 0
@@ -409,9 +445,20 @@ class Module:
             elif urgent and "urgent" not in item:
                 item["urgent"] = urgent
 
+            # set background and border colors. set left/right border widths
+            # only on first/last composites and no border width for inner
+            # composites or we will see 2px border lines between composites.
+            for key, value in self.i3bar_gaps_module_options.items():
+                if (key == "border_left" and index != 0) or (
+                    key == "border_right" and index != composite_length
+                ):
+                    item[key] = 0
+                else:
+                    item[key] = value
+
         # set min_width
-        if "min_width" in self.py3_module_options:
-            min_width = self.py3_module_options["min_width"]
+        if "min_width" in self.py3status_module_options:
+            min_width = self.py3status_module_options["min_width"]
 
             # get width, skip if width exceeds min_width
             width = sum([len(x["full_text"]) for x in response["composite"]])
@@ -424,7 +471,7 @@ class Module:
             offset = min_width - ((padding * 2) + width)
 
             # set align
-            align = self.py3_module_options.get("align", "left")
+            align = self.py3status_module_options.get("align", "left")
             if align == "center":
                 left = right = " " * padding
                 if self.random_int:
@@ -801,7 +848,7 @@ class Module:
                         if not self.allow_urgent and "urgent" in result:
                             del result["urgent"]
                         # set universal module options in result
-                        result.update(self.i3s_module_options)
+                        result.update(self.i3bar_module_options)
 
                     result["instance"] = self.module_inst
                     result["name"] = self.module_name
